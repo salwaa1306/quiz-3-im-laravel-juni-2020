fe32b38afc027c9791622f62a8f2e6bb1f5e73f3
unknown <wahyu.moeslim@gmail.com>
2020-07-05 15:02:10 +0700

    2. Membuat migrations

---
+++ database/migrations/2020_07_05_075659_create_followers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nm_follower');
+            $table->string('email_follower')->nullable();
+            $table->string('photo_followers')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+}
